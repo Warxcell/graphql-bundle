a9539e8fac147d16daa4bbbd349e69998f5e52ca
Bozhidar Hristov <warxcell@gmail.com>
2023-10-16 16:04:37 +0300

    fix types

--- src/ErrorHandlerInterface.php
+++ src/ErrorHandlerInterface.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace Arxy\GraphQL;
 
 use Closure;
+use GraphQL\Executor\ExecutionResult;
 use Throwable;
 
+/**
+ * @phpstan-import-type ErrorFormatter from ExecutionResult
+ * @phpstan-import-type SerializableErrors from ExecutionResult
+ */
 interface ErrorHandlerInterface
 {
     /**
      * @param list<Throwable> $errors
-     * @param Closure(Throwable): string[] $formatter
+     * @param ErrorFormatter $formatter
+     * @return SerializableErrors
      */
     public function handleErrors(array $errors, Closure $formatter): array;
 }
